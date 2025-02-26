d86808c926c8c6c954e475b9498fe50cde9fbba5
goinn00 <nickvangoinga@belastingdienst.nl>
2025-02-26 10:51:08 +0100

    keymap update

--- lua/custom/plugins/keymap.lua
+++ lua/custom/plugins/keymap.lua
@@ -1,6 +1,6 @@
 return {
   vim.keymap.set('n', '<C-S>', ':w<CR>', { desc = 'Save Buffer' }),
-
+  vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Buffer Delete' }),
   vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
   vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = 'Next Buffer' }),
   vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = 'Previous Buffer' }),
